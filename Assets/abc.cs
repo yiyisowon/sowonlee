893b22e20ef05f753da5e68800486c4c1d34e083
yiyisowon <109709760+yiyisowon@users.noreply.github.com>
2022-07-21 13:04:41 +0900

    firstcommit
    aaaa

---
+++ Assets/abc.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class abc : MonoBehaviour
+{
+    int scoreNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            scoreNum++;
+            this.GetComponent<Text>().text = "점수: " + scoreNum.ToString();
+            this.GetComponent<Text>().text = $"점수: {scoreNum}";
+        }
+    }
+}
